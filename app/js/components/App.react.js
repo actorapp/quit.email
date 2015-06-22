7fa582c88ff8bff5791cc1d6a95b06d6559c4ef1
Oleg Shilov <me@olegshilov.com>
2015-06-22 21:06:42 +0300

    update styles; new envelop image for retina displays; use absolute positioning instead flexbox grid; cleanup;

--- app/js/components/App.react.js
+++ app/js/components/App.react.js
@@ -44,39 +44,20 @@ var App = React.createClass({
     var joinLink = 'http://app.actor.im/join/' + this.state.token;
 
     if (!this.state.isLoading) {
-      var groupAvatar = null;
-
-      if (group.avatarUrl) {
-        groupAvatar = <img className="avatar avatar--huge" src={group.avatarUrl}/>;
-      }
-
-        return (
-          <div className="row center-xs middle-xs">
-            <section className="invite-new">
-              <div className="invite-new__body">
-                  <h3>Invite to {group.title}</h3>
-                <p>
-                  <strong>{inviter.name}</strong> invite you to our small <strong>team chat</strong>.
-                </p>
-                <a href={joinLink} className="">Join chat</a>
-                <footer>
-                  Greetings,<br/><strong>Actor Team</strong>
-                </footer>
-              </div>
-            </section>
-
-            <section className="invite">
-              <div className="invite__group">
-                {groupAvatar}
-                <h3>{group.title}</h3>
-              </div>
-              <div className="invite__from">
-                <p><img className="avatar" src={inviter.avatarUrl}/> {inviter.name} приглашает Вас</p>
-              </div>
-              <a href={joinLink} className="button button--primary button--wide">Присоединиться</a>
-            </section>
+      return (
+        <section className="invite">
+          <div className="invite__body">
+              <h3>Invite to {group.title}</h3>
+            <p>
+              <strong>{inviter.name}</strong> invite you to our small <strong>team chat</strong>.
+            </p>
+            <a href={joinLink}>Join chat</a>
+            <footer>
+              Greetings,<br/><strong>Actor Team</strong>
+            </footer>
           </div>
-        )
+        </section>
+      )
     } else {
       return (
         <span>Loading...</span>
