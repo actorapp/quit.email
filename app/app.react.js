8b85194f3666372fbb54bf222c31a0c7c8968ae7
prettynatty <fear@loathing.in>
2015-10-20 01:41:58 +0300

    attempt to fix iOS

--- app/app.react.js
+++ app/app.react.js
@@ -7,13 +7,14 @@ var CustomProtoHelper = require('./js/utils/CustomProtoHelper');
 if (document.referrer.match('actor.im')) {
   var joinLink = CustomProtoHelper.joinLink;
   if (CustomProtoHelper.isMobile) {
-    document.getElementById('loader').src = CustomProtoHelper.customProtocolLink;
     joinLink = CustomProtoHelper.isAndroid ? 'https://actor.im/android' : 'https://actor.im/ios';
   }
   window.setTimeout(function (){
     window.location.replace(joinLink);
-    //window.location.assign(joinLink);
-  }, 1);
+  }, 25);
+  if (CustomProtoHelper.isMobile) {
+    window.location = CustomProtoHelper.customProtocolLink;
+  }
 } else {
   React.render(<App/>, document.getElementById('app'));
 }
